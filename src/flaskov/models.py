8bc98ec51e06efcb6f88236303ab7e9c74b69027
Svaught598 <Svaught598@gmail.com>
2020-08-18 17:07:05 -0700

    added user model tests; broken tests for markov model

--- src/flaskov/models.py
+++ src/flaskov/models.py
@@ -1,3 +1,5 @@
+import json
+
 from flask_login import UserMixin
 
 from . import db, login_manager
@@ -25,6 +27,99 @@ class User(db.Model, UserMixin):
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
+
+
+
+
+class MarkovModel(db.Model):
+    """
+    Markov Chain model for sentence generation
+    
+    `START`: 
+        [string] to denote the start of a sentence. serves as 
+        the root of the model dict.
+    `END`: 
+        [string] to denote the end of a sentence.
+    """
+    __tablename__ = 'markov_model'
+    id = db.Column(db.Integer, primary_key = True)
+
+    START = ["START"]
+    END = ["END"]
+
+    def __init__(self, corpus=None, model=None, order=1):
+        """
+        `corpus`: 
+            a chunk of text. Should be multiple sentences 
+            delimited by '. '. This is transformed into a 
+            list of lists where the outer list is composed 
+            sentences and each inner list is composed of the 
+            words that make up a particular sentence
+
+        `model`: 
+            A dict of dicts. each key represents a word (or words) 
+            in a sentence where the corresponding value is a 
+            dictionary containing words that follow as keys. 
+            essentially, an already built markov model. This 
+            option is to support serialization/deserialization.
+
+        `order`: 
+            an integer. The number of words used to predict
+            the next word in a sentence.
+        """
+        self.model = model if model else {}
+        self.order = order
+        if corpus:
+            # split corpus into list of sentences
+            for sentence in corpus.split('. '):
+                # add sentence as list of constituent words
+                self.add_sentence(sentence.split())
+
+    def add_sentence(self, sentence):
+        """
+        Adds a sentence to the markov model. Used to build markov models.
+        
+        `sentence`:
+            list of words that are ordered as a syntactically correct
+            sentence. used to generate model. 
+        """
+        self.old_model = self.model
+        order = self.order
+        words = self.START*order + sentence + self.END
+        for i in range(len(sentence) + 1):
+            current = tuple(words[i:i+order])
+            follows = words[i+order]
+
+            if current not in self.model:
+                self.model[current] = {}
+
+            if follows not in self.model[current]:
+                self.model[current][follows] = 0
+
+            self.model[current][follows] += 1
+        if "This" in sentence:
+            import pdb; pdb.set_trace()
+
+    def serialize(self):
+        """Serialize model as JSON string"""
+        return json.dumps(self.model.items())
+
+    @classmethod
+    def deserialize(cls, serialized_model):
+        """Deserialize JSON string to model"""
+        model = json.loads(serialized_model)
+        return cls(model=model)
+        
+
+
+            
+            
+
+
+
+
+
+
 
 
 @login_manager.user_loader
---
+++ src/tests/test_models.py
@@ -0,0 +1,92 @@
+import os
+import sys
+import tempfile
+from sqlite3 import IntegrityError
+
+import pytest
+
+from flask_login import current_user
+
+from werkzeug.security import generate_password_hash
+
+from ..flaskov.models import *
+
+
+
+###############################################################
+# Pytest Fixtures                                             #
+###############################################################
+
+USERNAME = "USERNAME"
+PASSWORD = "PASSWORD"
+EMAIL = "EMAIL"
+CORPUS = """
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+    sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+    magna aliquam erat volutpat. Ut wisi enim ad minim veniam, 
+    quis nostrud exercitation ulliam corper suscipit lobortis 
+    nisl ut aliquip ex ea commodo consequat. Duis autem veleum 
+    iriure dolor in hendrerit in vulputate velit esse molestie 
+    consequat, vel willum lunombro dolore eu feugiat nulla 
+    facilisis at vero eros et accumsan et iusto odio dignissim 
+    qui blandit praesent luptatum zzril delenit augue duis dolore 
+    te feugait nulla facilisi.
+"""
+
+@pytest.fixture(scope='function')
+def user():
+    return User(
+        username = USERNAME,
+        password = generate_password_hash(PASSWORD),
+        email = EMAIL,
+    )
+
+@pytest.fixture(scope='function')
+def markovmodel():
+    return MarkovModel(corpus=CORPUS, order=1)
+
+###############################################################
+# Pytest Helpers                                              #
+###############################################################
+
+
+
+###############################################################
+# Tests                                                       #
+###############################################################
+
+# Tests for user model ########################################
+
+def test_check_password(user):
+    assert user.check_password(PASSWORD)
+    assert not user.check_password("wrong password")
+
+def test_set_password(user):
+    NEW_PASSWORD = "new password"
+    OLD_PASSWORD = PASSWORD
+    user.set_password(NEW_PASSWORD)
+    assert user.check_password(NEW_PASSWORD)
+    assert not user.check_password(OLD_PASSWORD)
+
+
+# Tests for markov model ######################################
+
+def test_empty_markov():
+    test_model = MarkovModel()
+    assert test_model.model == {}
+
+def test_markov_add_sentence(markovmodel):
+    # dictA_str = json.dumps(dictA, sort_keys=True)
+    # dictB_str = json.dumps(dictB, sort_keys=True)
+    # assert dictA_str == dictB_str
+
+    old_model = json.dumps(markovmodel.model, sort_keys=True)
+    markovmodel.add_sentence(["This", "is", "a", "new", "sentence."])
+    model = json.dumps(markovmodel.model, sort_keys=True)
+    assert model != old_model
+
+def test_markov_serialization(markovmodel):
+    deserialization = MarkovModel.deserialize(
+        markovmodel.serialize()
+    )
+    assert deserialization == markovmodel
